e1209fb0b82f6837291a086e25fcdd7e51dab09d
alexsilva <geniofuturo@gmail.com>
2023-07-11 20:46:27 -0300

    Update test runner.

--- tests/runtests.py
+++ tests/runtests.py
@@ -2,20 +2,24 @@
 import os
 import sys
 
-from django.conf import settings
+import django
 from django.core import management
 
 
-def runtests():
-    pkg_path = os.path.normpath(
-        os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)
-    )
-    if pkg_path not in sys.path:
-        sys.path.insert(0, pkg_path)
-    
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
-    management.call_command('test', 'testapp')
+def run_tests():
+	pkg_path = os.path.normpath(
+		os.path.join(os.path.abspath(os.path.dirname(__file__)), os.pardir)
+	)
+	if pkg_path not in sys.path:
+		sys.path.insert(0, pkg_path)
+
+	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings')
+
+	# django setup apps
+	django.setup()
+
+	management.call_command('test', 'testapp')
 
 
 if __name__ == '__main__':
-    runtests()
+	run_tests()
